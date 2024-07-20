2e1e0dc7720b1340b2e33e1c13985e70104f606b
aswanth6000 <aswanth682@gmail.com>
2024-07-20 12:06:08 +0530

    user stored in db

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,8 +7,7 @@ export class AuthController {
   constructor(private authService: AuthService) {}
   @Post('signup')
   signup(@Body() dto: AuthDto) {
-    console.log(dto);
-    this.authService.signup();
+    this.authService.signup(dto);
   }
 
   @Post('signin')
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signin() {
-    return 'Iam sign in';
+  async signup(dto: AuthDto) {
+    // generate the hash
+    const hash = await argon.hash(dto.password);
+
+    //save to db
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+    });
+    delete user.hash;
+    //return the saved user
+    return user;
   }
 
-  signup() {
-    return 'Iam sign up';
+  signin() {
+    return 'Iam sign in';
   }
 }
